191d8c0c6423d1eb0de26908759d7e5d6bdedc58
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-09-26 18:20:30 +0500

    Very Easy Question 2

--- Very Easy/01_two_numbers_sum.py
+++ Very Easy/01_two_numbers_sum.py
@@ -1,2 +1,6 @@
 def addition(a, b):
     return a + b
+
+print(addition(3, 2))
+print(addition(-3, -6))
+print(addition(7, 3))
---
+++ Very Easy/02_next_integer.py
@@ -0,0 +1,6 @@
+def addition(num):
+    return num + 1
+
+print(addition(0))
+print(addition(9))
+print(addition(-3))
