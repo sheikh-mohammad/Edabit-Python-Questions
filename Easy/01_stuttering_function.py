3e90facf7aa80e6f1539c9ad36c3f58b999c8839
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-10-08 07:12:36 +0500

    Added Indentation in Q1 Easy

--- Easy/01_stuttering_function.py
+++ Easy/01_stuttering_function.py
@@ -1,6 +1,7 @@
-def stutter(word:str)->str:
+def stutter(word: str) -> str:
     word = word.lower()
     return f"{word[0:2]}... {word[0:2]}... {word}?"
+
 
 print(stutter("incredible"))
 print(stutter("enthusiastic"))
