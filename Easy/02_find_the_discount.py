dbb76faaad5fdd973200e009d907e91d68bf9e6e
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-10-08 07:09:24 +0500

    Added Type Hinting and Example in Q2 Easy

--- Easy/02_find_the_discount.py
+++ Easy/02_find_the_discount.py
@@ -1,4 +1,8 @@
-def dis(price, discount):
+def dis(price : int, discount : int | float) -> int | float:
     final_price = price - (price * discount/100)
     
     return round(final_price, 2)
+
+print(dis(1500, 50))
+print(dis(89, 20))
+print(dis(100, 75))
