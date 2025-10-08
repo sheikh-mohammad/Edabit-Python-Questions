e18f8aef61d8d0a267c21bbf08ad00184d8f1757
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-10-08 07:12:04 +0500

    Updated Q2 Easy

--- Easy/02_find_the_discount.py
+++ Easy/02_find_the_discount.py
@@ -1,7 +1,10 @@
-def dis(price : int, discount : int | float) -> int | float:
-    final_price = price - (price * discount/100)
-    
-    return round(final_price, 2)
+def dis(price: int, discount: int | float) -> int | float:
+    final_price = price - (price * discount / 100)
+    if final_price.is_integer():
+        return round(final_price, 0)
+    else:
+        return round(final_price, 2)
+
 
 print(dis(1500, 50))
 print(dis(89, 20))
