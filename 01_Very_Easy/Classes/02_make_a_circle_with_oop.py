bec3b4f6c8220b98092f0f708a34c971fedd10a9
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-10-28 20:18:45 +0500

    Added 02 Question in Classes of Very Easy

---
+++ 01_Very_Easy/Classes/02_make_a_circle_with_oop.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Circle:
+    def __init__(self, radius: int | float) -> None:
+        self.radius: int | float = radius
+
+    def getArea(self) -> int | float:
+        return math.pi * (self.radius**2)
+
+    def getPerimeter(self) -> int | float:
+        return 2 * math.pi * self.radius
+
+
+circy1 = Circle(11)
+print(circy1.getArea())
+
+circy2 = Circle(4.44)
+print(circy2.getPerimeter())
