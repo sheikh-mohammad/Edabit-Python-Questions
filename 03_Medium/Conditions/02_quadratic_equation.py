797ffd75513bcfa9c22029dd9b11811cdb4ef2e6
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-13 09:11:26 +0500

    Update solution for quadratic equation problem

---
+++ 03_Medium/Conditions/02_quadratic_equation.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+def quadratic_equation(a, b, c):
+    x = (-b + sqrt((b**2) - (4 * a * c))) / 2 * a
+    quad = (a * (x**2)) + b * x + c
+    
+    if (quad == 0):
+        return x
+    
+print(quadratic_equation(1, 2, -3))
+print(quadratic_equation(2, -7, 3))
+print(quadratic_equation(1, -12, -28))
