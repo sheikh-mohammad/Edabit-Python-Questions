77258de82abfca4b23fce3263aeed50fba7012d2
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-21 11:26:26 +0500

    Added program code in 01_boolean_to_string_convert.py

--- 01_very_easy/Conditions/01_boolean_to_string_convert.py
+++ 01_very_easy/Conditions/01_boolean_to_string_convert.py
@@ -1,1 +1,9 @@
+def bool_to_string(flag: bool) -> str:
+    if flag == True:
+        return "True"
+    elif flag == False:
+        return "False"
 
+
+print(bool_to_string(True))
+print(bool_to_string(False))
