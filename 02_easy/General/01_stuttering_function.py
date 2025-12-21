90dac231a12c4a318e498c708b2948f78e154745
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-21 08:09:44 +0500

    Update and rename 01_stuttering_function.py to 01_stuttering_function.py

--- 02_easy/General/01_stuttering_function.py
+++ 02_easy/General/01_stuttering_function.py
@@ -2,7 +2,6 @@ def stutter(word: str) -> str:
     word = word.lower()
     return f"{word[0:2]}... {word[0:2]}... {word}?"
 
-
 print(stutter("incredible"))
 print(stutter("enthusiastic"))
 print(stutter("outstanding"))
